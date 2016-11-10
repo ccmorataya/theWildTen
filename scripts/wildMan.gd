35c2a833ff7fd7cee3b01e1eadeb2a3ce8bbb91d
Cristian Morataya <cris.morataya@gmail.com>
2016-11-10 16:56:54 -0600

    Add code to move the player

--- scripts/wildMan.gd
+++ scripts/wildMan.gd
@@ -2,6 +2,8 @@
 extends Node2D
 
 const GRAVITY = 200.0
+const WALK_SPEED = 200
+
 var velocity = Vector2()
 
 func _ready():
@@ -9,6 +11,13 @@ func _ready():
 
 func _fixed_process(delta):
 	velocity.y += delta * GRAVITY
+
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x = -WALK_SPEED
+	elif (Input.is_action_pressed("ui_right")):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
 
 	var motion = velocity * delta
 	get_node("KinematicBody2D").move(motion)

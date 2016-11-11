17885b24fec498a5712a1fc7fe40bd3a99b1c6ab
Cristian Morataya <cris.morataya@gmail.com>
2016-11-11 11:11:54 -0600

    Add the jumping effect

--- scripts/wildMan.gd
+++ scripts/wildMan.gd
@@ -1,7 +1,7 @@
 
 extends Node2D
 
-const GRAVITY = 100
+const GRAVITY = 1000
 const WALK_SPEED = 200
 
 var velocity = Vector2()
@@ -11,6 +11,9 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	var inGround = true
+	var playerPosition = player.get_pos()
+	var jumpPostion = Vector2()
 	velocity.y += delta * GRAVITY
 
 	if (Input.is_action_pressed("ui_left")):
@@ -27,7 +30,15 @@ func _fixed_process(delta):
 	#if (player is colliding):
 		#velocity.y = 0
 	if (player.is_colliding()):
+		inGround = true
 		var n = player.get_collision_normal()
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
 		player.move(motion)
+
+	if (inGround):
+		if (Input.is_action_pressed("ui_up")):
+			velocity.y -= 50
+			inGround = false
+	
+	print(playerPosition)
